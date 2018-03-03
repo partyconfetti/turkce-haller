124f9ec2bcf22431e49e0a11b33986cee76e0e43
partyconfetti <5325408+partyconfetti@users.noreply.github.com>
2018-03-03 23:46:41 +0300

    Init

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# turkce-haller
+# Türkçe Haller
+
+![npm](https://nodei.co/npm/turkce-haller.png?downloads=true&downloadRank=true&stars=true)
+
+## Kurulum
+`npm install turkce-haller --save`
+
+## Nasıl kullanırım
+```javascript
+const haller = require('turkce-haller');
+
+haller('Ankara', 'den') // Ankara’dan
+haller('Ankara', 'den', '\' ') // Ankara' dan
+```
+
+## API
+haller(isim, hal, **kesmeIsareti**)
+
+```kesmeIsareti``` İsteğe bağlı, default: ```’```
+
+## Ekler
+
+| Durum                            | Ekler  | Örnek      |
+|----------------------------------|--------|------------|
+| Yönelme Hali                     | -e     | Sercan’a   |
+| Belirtme Hali                    | -i     | Sercan’ı   |
+| Bulunma Hali                     | -de    | Sercan’da  |
+| Ayrılma Hali                     | -den   | Sercan’dan |
+| İyelik Eki                       | -m -n  | Sercan’ın  |
+
+## Lisans
+MIT
+
+## Thanks
+Based on [haller.js](https://github.com/sercaneraslan/haller.js)
+
+✨ – [Try pollme!](https://pollme.co)
---
+++ index.js
@@ -0,0 +1,37 @@
+const IYELIK = 'iyelik'
+const iHALI = 'i'
+const eHALI = 'e'
+const deHALI = 'de'
+const denHALI = 'den'
+const iEKLERI = 'ııiiuuüü'
+
+module.exports = (isim, hal, kesme = '’') => {
+	if(!isim || !hal) return ''
+
+	hal = hal.toLocaleLowerCase('tr');
+	const sonHarf = isim[isim.length - 1]
+	const istisna = ~~/[ei][^ıüoö]*[au]l$|alp$/.test(isim) * 2 // Sapkali harf istisnasi var mı kontrol eder Orn: Alp, Resul, Cemal... 0 veya 2 degeri doner
+	const sonSesli = isim.match(/[aıeiouöü]/g).pop()
+
+	// Ek in sesli harfine karar verir
+	let ek = (hal === IYELIK || hal === iHALI) ?  // iyelik veya i hali ise
+		// Son sesli harf aıeiouöü harflerinin hangisine denk geliyorsa o index numarasıyla iEkleri nin n'inci elemanı seçilir
+		iEKLERI[ 'aıeiouöü'.indexOf(sonSesli) + istisna ]
+		: // e, de veya den hali ise
+		// Son sesli harf a, ı, o veya u ise ek a (istisna var ise e ), e, i, ö veya ü ise ek e harfi
+		(/[aıou]/.test(sonSesli)) ? istisna ? 'e' : 'a' : 'e';
+
+	// Kaynastirma harflerini ekler
+	if(sonHarf === sonSesli)
+		ek = (hal === IYELIK) ? 'n' + ek : (hal === iHALI || hal === eHALI) ? 'y' + ek : ek
+
+	// Harf yumusamalarini kontrol eder
+	if(hal === deHALI || hal === denHALI)
+		ek = (/[fstkçşhp]/.test(sonHarf) ? 't' : 'd') + ek
+
+	// Iyelik veya den hali icin ek in sonuna n harfi ekler
+	if(hal === IYELIK || hal === denHALI)
+		ek += 'n'
+
+	return `${isim}${kesme}${ek}`
+}
